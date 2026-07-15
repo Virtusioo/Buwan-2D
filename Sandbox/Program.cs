0d12a3448c1b71d4285c10cb654883679fcefa5e
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-15 21:35:41 +0800

    Add Sandbox project for experimentation

---
+++ Sandbox/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Lua;
+using Lua.Standard;
+
+internal class Program
+{
+    static LuaState lua;
+
+    static async void ExecuteLuaCode()
+    {
+        lua.Environment["Log"] = new LuaFunction((context, ct) =>
+        {
+            foreach (LuaValue value in context.Arguments)
+            {
+                Console.Write($"{value} ");
+            }
+
+            Console.WriteLine();
+
+            return new(0);
+        });
+
+        try
+        {
+            var value = await lua.DoStringAsync("print('Hello, World')");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        lua = LuaState.Create();
+
+        lua.OpenStandardLibraries();
+
+        ExecuteLuaCode();
+    }
+}
