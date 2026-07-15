7b1b6c68455e3c56ed9d6fa8cedc6a5c1cadb615
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-16 06:42:34 +0800

    Rewrote Program class

--- Buwan/Program.cs
+++ Buwan/Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿
+using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World");
     }
 }
