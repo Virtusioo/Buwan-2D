83018d76716bbe9fbbb2dc79544bda5d8984d6b6
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-16 21:14:49 +0800

    Add SDLException class for SDL exceptions

---
+++ Buwan/Exceptions/SDLException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buwan.Exceptions
+{
+    internal class SDLException : Exception
+    {
+        public SDLException(string message) 
+            : base(message)
+        {}
+    }
+}
