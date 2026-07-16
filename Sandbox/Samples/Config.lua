7f0347e8fbde2e71e680cda7fe33de6acccea3f8
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-16 21:18:29 +0800

    Add ScreenWidth and ScreenHeight config attributes

--- Sandbox/Samples/Config.lua
+++ Sandbox/Samples/Config.lua
@@ -1,8 +1,10 @@
 
 function App.GetConfig()
 	return {
-		AppName = "Basic",
-		AppVersion = "1.0.0",
-		AppIdentifier = "com.buwan.basic"
+		Name = "Basic",
+		Version = "1.0.0",
+		Identifier = "com.buwan.basic"
+		ScreenWidth = 800,
+		ScreenHeight = 600
 	}
 end
