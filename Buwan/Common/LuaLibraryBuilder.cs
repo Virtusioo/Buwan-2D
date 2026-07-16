ecfb9d792d2378ef1ffa3277d6d3cce0496ec349
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-16 14:25:45 +0800

    Add LuaLibraryBuilder functionality

--- Buwan/Common/LuaLibraryBuilder.cs
+++ Buwan/Common/LuaLibraryBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Buwan.Model;
+using Lua;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,24 @@ namespace Buwan.Common
 {
     internal class LuaLibraryBuilder
     {
+        private readonly Dictionary<string, ILuaModule> _modules = [];
 
+        public void PutModule(string name, ILuaModule module)
+        {
+            _modules[name] = module;
+        }
+
+        public void BuildToTable(LuaTable library)
+        {
+            foreach (var pair in _modules)
+            {
+                string name = pair.Key;
+                ILuaModule module = pair.Value;
+                LuaTable tableModule = new();
+
+                module.OnCreate(tableModule);
+                library[name] = tableModule;
+            }
+        }
     }
 }
