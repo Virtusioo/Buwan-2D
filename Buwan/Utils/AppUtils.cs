bab7668e46c2a229abf4a1dbef8fde508d95a5b7
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-16 21:15:22 +0800

    Add AppUtils class for showing dialog boxes

---
+++ Buwan/Utils/AppUtils.cs
@@ -0,0 +1,20 @@
+﻿using SDL3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buwan.Utils
+{
+    internal class AppUtils
+    {
+        public static void ShowErrorBox(string message)
+        {
+            SDL.ShowSimpleMessageBox(SDL.MessageBoxFlags.Error,
+                                     "Error",
+                                     message,
+                                     IntPtr.Zero);
+
+            SDL.Log($"Error: {message}");
+        }
+    }
+}
