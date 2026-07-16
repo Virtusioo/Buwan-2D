8c49f573c53361ae6b62a868c875f244d3e06a0c
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-16 14:26:01 +0800

    Rename Model to Models

---
+++ Buwan/Models/ILuaModule.cs
@@ -0,0 +1,12 @@
+﻿using Lua;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buwan.Model
+{
+    internal interface ILuaModule
+    {
+        void OnCreate(LuaTable module);
+    }
+}
