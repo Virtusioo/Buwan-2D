ebe4f6c12b6edede513774bb856c2dba178f2c86
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-16 15:41:52 +0800

    Add Renderer and Window attributes

---
+++ Buwan/Runtime/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SDL3;
+
+namespace Buwan.Runtime
+{
+    internal class Application
+    {
+        public uint Renderer { get; private set; }
+        public uint Window { get; private set; }
+
+
+    }
+}
