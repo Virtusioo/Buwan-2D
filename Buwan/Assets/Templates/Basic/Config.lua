2ddb105508fb18fb40597154352dabedd5309737
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-17 10:49:27 +0800

    Add basic lua template

---
+++ Buwan/Assets/Templates/Basic/Config.lua
@@ -0,0 +1,16 @@
+
+
+-- Get the default app configuration 
+---@returns The default app configuration table
+function App.GetConfig()
+	return {
+		-- App metadata --
+		Name = "Basic", -- The name of the app 
+		Version = "1.0.0", -- The version of the app: MAJOR.MINOR.PATCH
+		Identifier = "com.buwan.basic" -- The identifier of the app (e.g. com.companyname.appname)
+
+		-- Screen defaults --
+		ScreenWidth = 800, -- The initial window width 
+		ScreenHeight = 600 -- The initial window height
+	}
+end 
---
+++ Buwan/Assets/Templates/Basic/Main.lua
@@ -0,0 +1,18 @@
+
+
+-- Runs after successful window creation
+---@note You can remove this function 
+function App.OnReady()
+	
+end
+
+-- Runs on every app update
+---@param deltaTime How long the last frame took in seconds
+function App.OnUpdate(deltaTime)
+	
+end
+
+-- Runs on every app draw
+function App.OnDraw()
+	
+end
