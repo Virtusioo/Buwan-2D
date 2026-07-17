e2b253b3039ad3790fa393e9e25ddeaf1c302fa7
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-17 14:10:09 +0800

    Update documentation

--- Buwan/Assets/Templates/Basic/Main.lua
+++ Buwan/Assets/Templates/Basic/Main.lua
@@ -3,16 +3,16 @@
 -- Runs after successful window creation
 ---@note You can remove this function 
 function App.OnReady()
-	
+	-- Initialize stuff here
 end
 
 -- Runs on every app update
 ---@param deltaTime How long the last frame took in seconds
 function App.OnUpdate(deltaTime)
-	
+	-- Update stuff here
 end
 
 -- Runs on every app draw
 function App.OnDraw()
-	
+	-- Draw stuff here
 end
