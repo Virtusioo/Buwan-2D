795266aaf5297e7bdc303188b2a4434c9870c1c1
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-17 10:12:02 +0800

    Add basic lua colors

--- Buwan/Modules/ColorsModule.cs
+++ Buwan/Modules/ColorsModule.cs
@@ -6,11 +6,48 @@ using System.Text;
 
 namespace Buwan.Modules
 {
+    [LuaObject]
+    internal partial class LuaColor
+    {
+        [LuaMember("R")]
+        public float R;
+
+        [LuaMember("G")]
+        public float G;
+
+        [LuaMember("B")]
+        public float B;
+
+        public LuaColor() { }
+
+        public LuaColor(float r, float g, float b)
+        {
+            R = r;  
+            G = g;
+            B = b;
+        }
+
+        [LuaMember("FromRGB")]
+        public static LuaColor FromRGB(float r, float g, float b)
+        {
+            return new LuaColor
+            {
+                R = r / 255,
+                G = g / 255,
+                B = b / 255
+            };
+        }
+    }
+
     internal class ColorsModule : ILuaModule
     {
         public void OnCreate(LuaTable module)
         {
-            throw new NotImplementedException("Implement ColorsModule");
+            module["Black"] = new LuaColor(0, 0, 0);
+            module["White"] = new LuaColor(1, 1, 1);
+            module["Red"] = new LuaColor(1, 0, 0);
+            module["Blue"] = new LuaColor(0, 1, 0);
+            module["Green"] = new LuaColor(0, 0, 1);
         }
     }
 }
