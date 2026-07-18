5b0607e66b2e06049870d1c36d90815423afb680
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-18 13:27:28 +0800

    Change 'myRectangle' to 'MyRectangle'

--- Buwan/Assets/Templates/Basic/Main.lua
+++ Buwan/Assets/Templates/Basic/Main.lua
@@ -1,19 +1,19 @@
 
-local myRectangle
+local MyRectangle
 local RectangleSpeed = 60
 
 -- Runs after every module has initialized (e.g., Graphics, Colors).
 -- You can remove this function 
 function App.OnReady()
 	-- Initialize stuff here
-	myRectangle = Rectangle.new(10, 10, 100, 100)
+	MyRectangle = Rectangle.new(10, 10, 100, 100)
 end
 
 -- Runs on every app update
 ---@param deltaTime number How long the last frame took in seconds
 function App.OnUpdate(deltaTime)
 	-- Update stuff here
-	myRectangle.X = myRectangle.X + RectangleSpeed * deltaTime
+	MyRectangle.X = MyRectangle.X + RectangleSpeed * deltaTime
 end
 
 -- Runs on every app draw
@@ -21,5 +21,5 @@ function App.OnDraw()
 	-- Draw stuff here
 	Graphics.ClearScreen()
 	Graphics.SetColor(Colors.Red)
-	Graphics.DrawRectangle(myRectangle)
+	Graphics.DrawRectangle(MyRectangle)
 end
