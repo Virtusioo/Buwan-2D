d2494be58eaa5a19efc527ad9ca73ec80a27040d
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-18 15:33:18 +0800

    Add .Name attribute

---
+++ Buwan/Models/BuwanModule.cs
@@ -0,0 +1,13 @@
+﻿using Lua;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buwan.Models
+{
+    internal abstract class BuwanModule(string name)
+    {
+        public readonly string Name = name;
+        public abstract void OnCreate(LuaTable module);
+    }
+}
