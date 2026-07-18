f04a6233db891f3d7bc0ae7f14eb125e69dbaa02
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-18 21:14:24 +0800

    Add new AppModule for exitting the app

---
+++ Buwan/Modules/AppModule.cs
@@ -0,0 +1,27 @@
+﻿using Buwan.Models;
+using Lua;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buwan.Modules
+{
+    internal class AppModule : BuwanModule
+    {
+        public bool IsRunning = false;
+
+        public AppModule()
+            : base("App")
+        {}
+
+        public override void OnCreate(LuaTable module)
+        {
+            module["Exit"] = new LuaFunction((context, ct) =>
+            {
+                IsRunning = false;
+
+                return new(0);
+            });
+        }
+    }
+}
