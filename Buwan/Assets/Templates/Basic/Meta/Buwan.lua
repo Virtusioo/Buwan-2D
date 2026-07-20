b92c2d4e22757fb238fb56571938b44d96fea62b
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-21 05:35:42 +0800

    Improve API documentation

--- Buwan/Assets/Templates/Basic/Meta/Buwan.lua
+++ Buwan/Assets/Templates/Basic/Meta/Buwan.lua
@@ -71,6 +71,7 @@ Vector2 = {}
 -- Create a new Vector2 object 
 ---@param x number The horizontal position of the vector (or point)
 ---@param y number The vertical position of the vector (or point)
+---@return Vector2
 function Vector2.new(x, y) end
 
 Graphics = {}
@@ -80,7 +81,7 @@ function Graphics:ClearScreen() end
 
 -- Begin properties state for drawing
 --
--- These properties include rotation, color, alpha, etc
+-- These properties include color, alpha, etc
 function Graphics:BeginState() end
 
 -- End properties state for drawing 
@@ -100,3 +101,29 @@ function Graphics:SetColor(color) end
 -- Draw a rectangle 
 ---@param rectangle Rectangle The rectangle to use 
 function Graphics:DrawRectangle(rectangle) end
+
+-- Draw a line 
+---@param startPoint Vector2 The start point of the line
+---@param endPoint Vector2 The end point of the line 
+function Graphics:DrawLine(startPoint, endPoint) end
+
+-- Draw multiple connected lines 
+---@param points Vector2[] The points to connect
+function Graphics:DrawLines(points) end
+
+-- Draw a point 
+---@param point Vector2 
+function Graphics:DrawPoint(point) end
+
+-- Draw debug text 
+---@param text string The debug text to draw 
+---@param position Vector2 The position of the debug text
+function Graphics:DrawDebugText(text, position) end
+
+-- Set the drawing scale for every drawing operation
+---@param scaleFactor Vector2 The scale to multiply (X refers to width, Y refers to height)
+function Graphics:SetScale(scaleFactor) end
+
+-- Set the color scale (useful as a brightness factor)
+---@param scaleFactor number The scale to multiply
+function Graphics:SetColorScale(scaleFactor) end
