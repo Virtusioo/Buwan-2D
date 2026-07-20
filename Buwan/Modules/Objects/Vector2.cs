126dd1e5e2d5515c72f9fef86041a4c0084c0edf
David <virtusiodavidrafael7.snfss@gmail.com>
2026-07-21 05:36:19 +0800

    Make Vector2 compose SDL.FPoint

--- Buwan/Modules/Objects/Vector2.cs
+++ Buwan/Modules/Objects/Vector2.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 using Lua;
+using SDL3;
 
 namespace Buwan.Modules.Objects
 {
     [LuaObject]
     internal partial class Vector2
     {
+        public SDL.FPoint Point;
+
         [LuaMember("X")]
-        public float X;
+        public float X
+        {
+            get => Point.X;
+            set => Point.X = value;
+        }
 
         [LuaMember("Y")]
-        public float Y;
+        public float Y
+        {
+            get => Point.Y;
+            set => Point.Y = value;
+        }
 
         [LuaMember("new")]
         public static Vector2 New(float x, float y)
@@ -23,5 +34,7 @@ namespace Buwan.Modules.Objects
                 Y = y
             };
         }
+
+        public static implicit operator SDL.FPoint(Vector2 value) => value.Point;
     }
 }
